33a8eba8afd5635378992cc0e97647fcca00075a
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-08-27 21:14:03 +0200

    Keep working on python scripts to further analyze simulations. I have to use the B (Bio) functions, not these ones. Code in temporary state.

--- scripts/Pareto_front.py
+++ scripts/Pareto_front.py
@@ -1,11 +1,10 @@
 import numpy as np
-
 from scipy.optimize import minimize
-
 from tqdm import tqdm
+from typing import Tuple, List
 
 
-def check_dists_0(p_x, r_x):
+def check_dists_0(p_x: np.ndarray, r_x: np.ndarray) -> int:
     n_x = p_x.shape[1]
     assert p_x.shape[0] == 1
     assert r_x.shape[0] == 1
@@ -17,7 +16,7 @@ def check_dists_0(p_x, r_x):
     return n_x
 
 
-def check_p_sgx(p_sgx, n_x):
+def check_p_sgx(p_sgx: np.ndarray, n_x: int) -> int:
     n_s = p_sgx.shape[0]
     assert p_sgx.shape[1] == n_x
     assert np.all(p_sgx > 0)
@@ -25,69 +24,70 @@ def check_p_sgx(p_sgx, n_x):
     return n_s
 
 
-def check_dists(p_x, p_sgx, r_x):
+def check_dists(p_x: np.ndarray, p_sgx: np.ndarray, r_x: np.ndarray) -> Tuple[int, int]:
     n_x = check_dists_0(p_x, r_x)
     n_s = check_p_sgx(p_sgx, n_x)
     return n_x, n_s
 
 
-def calc_aux_dists(p_sgx, p_x):
+def calc_aux_dists(
+    p_sgx: np.ndarray, p_x: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     p_x_s = p_sgx * p_x
     p_s = np.sum(p_x_s, axis=1, keepdims=True)
     p_xgs = p_x_s / p_s
     return p_x_s, p_s, p_xgs
 
 
-def calc_gamma_0(r_x, p_x):
+def calc_gamma_0(r_x: np.ndarray, p_x: np.ndarray) -> float:
     q_x = r_x / p_x
     gamma_0 = np.sqrt(np.sum(p_x * q_x**2) - 1)
     return gamma_0
 
 
-def calc_gamma(r_x, p_xgs, p_s):
+def calc_gamma(r_x: np.ndarray, p_xgs: np.ndarray, p_s: np.ndarray) -> float:
     q_xgs = r_x / p_xgs
     avg_q2_xgs_s = np.sum(p_xgs * q_xgs**2, axis=1, keepdims=True)
     gamma = np.sqrt(1 / np.sum(p_s / avg_q2_xgs_s) - 1)
     return gamma
 
 
-def calc_Pareto_front_W(p_x, p_sgx, r_x):
+def calc_Pareto_front_W(
+    p_x: np.ndarray, p_sgx: np.ndarray, r_x: np.ndarray
+) -> List[List[np.ndarray] | List[float]]:
     n_x, n_s = check_dists(p_x, p_sgx, r_x)
     p_x_s, p_s, p_xgs = calc_aux_dists(p_sgx, p_x)
     gamma = calc_gamma(r_x, p_xgs, p_s)
 
-    def avg_W(b_xgs):
+    def avg_W(b_xgs: np.ndarray) -> float:
         return np.sum(p_x_s * np.log(b_xgs / r_x))
 
-    def sig_W(b_xgs):
+    def sig_W(b_xgs: np.ndarray) -> float:
         return np.sqrt(np.sum(p_x_s * (np.log(b_xgs / r_x) ** 2)) - avg_W(b_xgs) ** 2)
 
-    def obj_func(b_xgs_f, alpha):
+    def obj_func(b_xgs_f: np.ndarray, alpha: float) -> float:
         b_xgs = np.reshape(b_xgs_f, (n_s, n_x))
         return -(alpha * avg_W(b_xgs) - (1 - alpha) * sig_W(b_xgs))
 
-    def gen_g_s(i_s):
-        def g_s(b_xgs_f):
+    def gen_g_s(i_s: int):
+        def g_s(b_xgs_f: np.ndarray) -> float:
             b_xgs = np.reshape(b_xgs_f, (n_s, n_x))
             return np.sum(b_xgs[i_s]) - 1
 
         return g_s
 
     bnds = tuple((0, 1) for _ in range(n_s * n_x))
-
     g_s_l = [gen_g_s(i_s) for i_s in range(n_s)]
     cons = [{"type": "eq", "fun": g_s} for g_s in g_s_l]
 
     b_xgs_o = p_xgs
     b_xgs_f_i = b_xgs_o.flatten()
 
-    alpha_l = list(
-        np.linspace(1, 1 / (1 + gamma), num=1024, endpoint=False),
-    )
+    alpha_l = list(np.linspace(1, 1 / (1 + gamma), num=1024, endpoint=False))
 
-    b_xgs_l = []
-    avg_W_l = []
-    sig_W_l = []
+    b_xgs_l: List[np.ndarray] = []
+    avg_W_l: List[float] = []
+    sig_W_l: List[float] = []
 
     for alpha in tqdm(alpha_l, ncols=80):
         res = minimize(
@@ -106,6 +106,5 @@ def calc_Pareto_front_W(p_x, p_sgx, r_x):
         avg_W_l.append(avg_W(b_xgs))
         sig_W_l.append(sig_W(b_xgs))
 
-    res_l = [b_xgs_l, avg_W_l, sig_W_l]
-
+    res_l: List[List[np.ndarray] | List[float]] = [b_xgs_l, avg_W_l, sig_W_l]
     return res_l
--- scripts/run_all_sims.py
+++ scripts/run_all_sims.py
@@ -1,7 +1,8 @@
 #!/home/marcomc/Documents/Doctorado/mutare/.venv/bin/python3
 
-from config import create_config, save_config
-from runner import mutare_create, mutare_resume, mutare_analyze, mutare_clean
+from config import create_config  # , save_config
+
+# from runner import mutare_create, mutare_resume, mutare_analyze, mutare_clean
 from results import read_results, print_results
 from Pareto_front import calc_aux_dists
 from Pareto_front import calc_Pareto_front_W
@@ -27,24 +28,68 @@ sim_dir.mkdir(parents=True, exist_ok=True)
 
 config = create_config()
 
-save_config(config, sim_dir)
+# save_config(config, sim_dir)
 
-mutare_clean(sim_dir)
+# mutare_clean(sim_dir)
 
-mutare_create(sim_dir)
-mutare_create(sim_dir)
+# mutare_create(sim_dir)
+# mutare_create(sim_dir)
 
-mutare_resume(sim_dir, 0)
-mutare_resume(sim_dir, 0)
+# mutare_resume(sim_dir, 0)
+# mutare_resume(sim_dir, 0)
 
-mutare_resume(sim_dir, 1)
-mutare_resume(sim_dir, 1)
+# mutare_resume(sim_dir, 1)
+# mutare_resume(sim_dir, 1)
 
-mutare_analyze(sim_dir)
+# mutare_analyze(sim_dir)
 
 print_results(sim_dir, 0)
 print_results(sim_dir, 1)
 
-p_x = read_results(sim_dir, 0)["prob_env"][0]["mean"]
-
+p_x = np.array([[stats["mean"] for stats in read_results(sim_dir, 0)["prob_env"]]])
 print(p_x)
+
+avg_prob_phe = np.array(
+    [[stats["mean"] for stats in read_results(sim_dir, 0)["avg_prob_phe"]]]
+)
+print(avg_prob_phe)
+
+p_sgx_0 = np.array([[0.5, 0.5], [0.5, 0.5]])
+
+# p_e = p_x
+# f_cge = np.exp((np.array(config["prob_rep"]) - np.array(config["prob_dec"])) / p_e)
+
+r_x = np.array([[0.75, 0.25]])  # placeholder, I have to use B (Bio) functions.
+
+b_xgs_l, avg_W_l, sig_W_l = calc_Pareto_front_W(p_x, p_sgx_0, r_x)
+print(b_xgs_l[0])
+
+fig, ax = plt.subplots()
+
+ax.plot(avg_W_l, sig_W_l, c="b")
+
+# tmp ------------------------------------------------------------
+
+p_x_s, p_s, p_xgs = calc_aux_dists(p_sgx_0, p_x)
+
+
+def avg_W(b_xgs: np.ndarray) -> float:
+    return np.sum(p_x_s * np.log(b_xgs / r_x))
+
+
+def sig_W(b_xgs: np.ndarray) -> float:
+    return np.sqrt(np.sum(p_x_s * (np.log(b_xgs / r_x) ** 2)) - avg_W(b_xgs) ** 2)
+
+
+avg_W_sim = avg_W(avg_prob_phe)
+sig_W_sim = sig_W(avg_prob_phe)
+
+print(avg_W_sim, sig_W_sim)
+
+# tmp ------------------------------------------------------------
+
+ax.scatter(avg_W_sim, sig_W_sim, c="r", s=20)
+
+fig.savefig("simulations/test-plot.pdf")
+
+plt.close(fig)
