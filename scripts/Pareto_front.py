e56b628b0e6fc8104d2c8092e11b810cc5e46617
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-08-28 11:58:56 +0200

    Compute theoretical Bio Pareto front: simulation results belong to the front!

--- scripts/Pareto_front.py
+++ scripts/Pareto_front.py
@@ -1,110 +1,156 @@
 import numpy as np
-from scipy.optimize import minimize
+
+from scipy.optimize import minimize, basinhopping
+
 from tqdm import tqdm
-from typing import Tuple, List
+
+from typing import Tuple, List, Callable
 
 
-def check_dists_0(p_x: np.ndarray, r_x: np.ndarray) -> int:
-    n_x = p_x.shape[1]
-    assert p_x.shape[0] == 1
-    assert r_x.shape[0] == 1
-    assert r_x.shape[1] == n_x
-    assert np.all(p_x > 0)
-    assert np.all(r_x > 0)
-    assert np.isclose(np.sum(p_x), 1)
-    assert np.isclose(np.sum(r_x), 1)
-    return n_x
+def check_p_e(p_e: np.ndarray) -> int:
+    n_e = p_e.shape[1]
+    assert p_e.shape[0] == 1
+    assert np.all(p_e > 0)
+    assert np.isclose(np.sum(p_e), 1)
+    return n_e
 
 
-def check_p_sgx(p_sgx: np.ndarray, n_x: int) -> int:
-    n_s = p_sgx.shape[0]
-    assert p_sgx.shape[1] == n_x
-    assert np.all(p_sgx > 0)
-    assert np.all(np.isclose(np.sum(p_sgx, axis=0), 1))
+def check_p_sge(p_sge: np.ndarray, n_e: int) -> int:
+    n_s = p_sge.shape[0]
+    assert p_sge.shape[1] == n_e
+    assert np.all(p_sge > 0)
+    assert np.all(np.isclose(np.sum(p_sge, axis=0), 1))
     return n_s
 
 
-def check_dists(p_x: np.ndarray, p_sgx: np.ndarray, r_x: np.ndarray) -> Tuple[int, int]:
-    n_x = check_dists_0(p_x, r_x)
-    n_s = check_p_sgx(p_sgx, n_x)
-    return n_x, n_s
+def check_f_cge(f_cge: np.ndarray, n_e: int) -> int:
+    n_c = f_cge.shape[0]
+    assert f_cge.shape[1] == n_e
+    assert np.all(f_cge > 0)
+    return n_c
 
 
-def calc_aux_dists(
-    p_sgx: np.ndarray, p_x: np.ndarray
-) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-    p_x_s = p_sgx * p_x
-    p_s = np.sum(p_x_s, axis=1, keepdims=True)
-    p_xgs = p_x_s / p_s
-    return p_x_s, p_s, p_xgs
+def check_dists(
+    p_e: np.ndarray, p_sge: np.ndarray, f_cge: np.ndarray
+) -> Tuple[int, int, int]:
+    n_e = check_p_e(p_e)
+    n_s = check_p_sge(p_sge, n_e)
+    n_c = check_f_cge(f_cge, n_e)
+    return n_e, n_s, n_c
 
 
-def calc_gamma_0(r_x: np.ndarray, p_x: np.ndarray) -> float:
-    q_x = r_x / p_x
-    gamma_0 = np.sqrt(np.sum(p_x * q_x**2) - 1)
-    return gamma_0
+def calc_Pareto_front(
+    p_e: np.ndarray, p_sge: np.ndarray, f_cge: np.ndarray
+) -> Tuple[List[np.ndarray], List[float], List[float]]:
+    _, n_s, n_c = check_dists(p_e, p_sge, f_cge)
 
+    prng = np.random.default_rng(1234)
 
-def calc_gamma(r_x: np.ndarray, p_xgs: np.ndarray, p_s: np.ndarray) -> float:
-    q_xgs = r_x / p_xgs
-    avg_q2_xgs_s = np.sum(p_xgs * q_xgs**2, axis=1, keepdims=True)
-    gamma = np.sqrt(1 / np.sum(p_s / avg_q2_xgs_s) - 1)
-    return gamma
+    def gen_random_b_cgs() -> np.ndarray:
+        b_cgs = prng.uniform(size=(n_c, n_s))
+        b_cgs = b_cgs / np.sum(b_cgs, axis=0, keepdims=True)
+        return b_cgs
 
+    def avg_W(b_cgs: np.ndarray) -> float:
+        b_cge = b_cgs @ p_sge
+        f_b_c = np.sum(f_cge * b_cge, axis=0, keepdims=True)
+        return np.sum(p_e * np.log(f_b_c))
 
-def calc_Pareto_front_W(
-    p_x: np.ndarray, p_sgx: np.ndarray, r_x: np.ndarray
-) -> List[List[np.ndarray] | List[float]]:
-    n_x, n_s = check_dists(p_x, p_sgx, r_x)
-    p_x_s, p_s, p_xgs = calc_aux_dists(p_sgx, p_x)
-    gamma = calc_gamma(r_x, p_xgs, p_s)
+    def sig_W(b_cgs: np.ndarray) -> float:
+        b_cge = b_cgs @ p_sge
+        f_b_c = np.sum(f_cge * b_cge, axis=0, keepdims=True)
+        return np.sqrt(np.sum(p_e * np.log(f_b_c) ** 2) - avg_W(b_cgs) ** 2)
 
-    def avg_W(b_xgs: np.ndarray) -> float:
-        return np.sum(p_x_s * np.log(b_xgs / r_x))
+    def obj_func(b_cgs_f: np.ndarray, alpha: float) -> float:
+        b_cgs = np.reshape(b_cgs_f, (n_c, n_s))
+        return -(alpha * avg_W(b_cgs) - (1 - alpha) * sig_W(b_cgs))
 
-    def sig_W(b_xgs: np.ndarray) -> float:
-        return np.sqrt(np.sum(p_x_s * (np.log(b_xgs / r_x) ** 2)) - avg_W(b_xgs) ** 2)
-
-    def obj_func(b_xgs_f: np.ndarray, alpha: float) -> float:
-        b_xgs = np.reshape(b_xgs_f, (n_s, n_x))
-        return -(alpha * avg_W(b_xgs) - (1 - alpha) * sig_W(b_xgs))
-
-    def gen_g_s(i_s: int):
-        def g_s(b_xgs_f: np.ndarray) -> float:
-            b_xgs = np.reshape(b_xgs_f, (n_s, n_x))
-            return np.sum(b_xgs[i_s]) - 1
+    def gen_g_s(i_s: int) -> Callable[[np.ndarray], float]:
+        def g_s(b_cgs_f: np.ndarray) -> float:
+            b_cgs = np.reshape(b_cgs_f, (n_c, n_s))
+            return np.sum(b_cgs[:, i_s]) - 1
 
         return g_s
 
-    bnds = tuple((0, 1) for _ in range(n_s * n_x))
+    bnds = tuple((0.0, 1.0) for _ in range(n_c * n_s))
+
     g_s_l = [gen_g_s(i_s) for i_s in range(n_s)]
     cons = [{"type": "eq", "fun": g_s} for g_s in g_s_l]
 
-    b_xgs_o = p_xgs
-    b_xgs_f_i = b_xgs_o.flatten()
+    b_cgs = gen_random_b_cgs()
+    b_cgs_f_i = b_cgs.flatten()
 
-    alpha_l = list(np.linspace(1, 1 / (1 + gamma), num=1024, endpoint=False))
+    alpha_l = list(np.linspace(1, 0, num=1024, endpoint=False))
 
-    b_xgs_l: List[np.ndarray] = []
+    class TakeStep:
+        def __call__(self, _: np.ndarray) -> np.ndarray:
+            b_cgs = gen_random_b_cgs()
+            b_cgs_f = b_cgs.flatten()
+            return b_cgs_f
+
+    take_step = TakeStep()
+
+    class ShowMessage:
+        def __init__(self) -> None:
+            self.f_min = np.inf
+            self.niter = 0
+            self.niter_eq = 0
+
+        def __call__(self, _: np.ndarray, f: float, accept: bool) -> None:
+            self.niter += 1
+            self.niter_eq += 1
+            if accept and f < self.f_min:
+                self.f_min = f
+                self.niter_eq = 0
+            print(
+                "f_min={: .16f}".format(self.f_min),
+                "niter={:04d}".format(self.niter),
+                "niter_eq={:04d}".format(self.niter_eq),
+                end="\r",
+            )
+
+    show_message = ShowMessage()
+
+    res = basinhopping(
+        obj_func,
+        b_cgs_f_i,
+        niter=1024,
+        minimizer_kwargs={
+            "args": (1,),
+            "method": "SLSQP",
+            "bounds": bnds,
+            "constraints": cons,
+            "tol": 4e-12,
+        },
+        take_step=take_step,
+        callback=show_message,
+        niter_success=256,
+        rng=prng,
+    )
+    b_cgs_f = res.x
+    b_cgs_f_i = b_cgs_f
+
+    print("")
+
+    b_cgs_l: List[np.ndarray] = []
     avg_W_l: List[float] = []
     sig_W_l: List[float] = []
 
     for alpha in tqdm(alpha_l, ncols=80):
         res = minimize(
             obj_func,
-            b_xgs_f_i,
+            b_cgs_f_i,
             args=(alpha,),
             method="SLSQP",
             bounds=bnds,
             constraints=cons,
             tol=4e-12,
         )
-        b_xgs_f = res.x
-        b_xgs_f_i = b_xgs_f
-        b_xgs = np.reshape(b_xgs_f, (n_s, n_x))
-        b_xgs_l.append(b_xgs)
-        avg_W_l.append(avg_W(b_xgs))
-        sig_W_l.append(sig_W(b_xgs))
+        b_cgs_f = res.x
+        b_cgs_f_i = b_cgs_f
+        b_cgs = np.reshape(b_cgs_f, (n_c, n_s))
+        b_cgs_l.append(b_cgs)
+        avg_W_l.append(avg_W(b_cgs))
+        sig_W_l.append(sig_W(b_cgs))
 
-    res_l: List[List[np.ndarray] | List[float]] = [b_xgs_l, avg_W_l, sig_W_l]
-    return res_l
+    return b_cgs_l, avg_W_l, sig_W_l
--- scripts/run_all_sims.py
+++ scripts/run_all_sims.py
@@ -4,8 +4,7 @@ from config import create_config  # , save_config
 
 # from runner import mutare_create, mutare_resume, mutare_analyze, mutare_clean
 from results import read_results, print_results
-from Pareto_front import calc_aux_dists
-from Pareto_front import calc_Pareto_front_W
+from Pareto_front import calc_Pareto_front
 
 import numpy as np
 from pathlib import Path
@@ -46,23 +45,20 @@ config = create_config()
 print_results(sim_dir, 0)
 print_results(sim_dir, 1)
 
-p_x = np.array([[stats["mean"] for stats in read_results(sim_dir, 0)["prob_env"]]])
-print(p_x)
+p_e = np.array([[stats["mean"] for stats in read_results(sim_dir, 0)["prob_env"]]])
+print(p_e)
 
 avg_prob_phe = np.array(
     [[stats["mean"] for stats in read_results(sim_dir, 0)["avg_prob_phe"]]]
 )
 print(avg_prob_phe)
 
-p_sgx_0 = np.array([[0.5, 0.5], [0.5, 0.5]])
+p_sge = np.array([[1.0, 1.0]])
 
-# p_e = p_x
-# f_cge = np.exp((np.array(config["prob_rep"]) - np.array(config["prob_dec"])) / p_e)
+f_cge = np.exp((np.array(config["prob_rep"]) - np.array(config["prob_dec"])) / p_e)
 
-r_x = np.array([[0.75, 0.25]])  # placeholder, I have to use B (Bio) functions.
-
-b_xgs_l, avg_W_l, sig_W_l = calc_Pareto_front_W(p_x, p_sgx_0, r_x)
-print(b_xgs_l[0])
+b_cgs_l, avg_W_l, sig_W_l = calc_Pareto_front(p_e, p_sge, f_cge)
+print(b_cgs_l[0])
 
 fig, ax = plt.subplots()
 
@@ -70,19 +66,21 @@ ax.plot(avg_W_l, sig_W_l, c="b")
 
 # tmp ------------------------------------------------------------
 
-p_x_s, p_s, p_xgs = calc_aux_dists(p_sgx_0, p_x)
+
+def avg_W(b_cgs: np.ndarray) -> float:
+    b_cge = b_cgs @ p_sge
+    f_b_c = np.sum(f_cge * b_cge, axis=0, keepdims=True)
+    return np.sum(p_e * np.log(f_b_c))
 
 
-def avg_W(b_xgs: np.ndarray) -> float:
-    return np.sum(p_x_s * np.log(b_xgs / r_x))
+def sig_W(b_cgs: np.ndarray) -> float:
+    b_cge = b_cgs @ p_sge
+    f_b_c = np.sum(f_cge * b_cge, axis=0, keepdims=True)
+    return np.sqrt(np.sum(p_e * np.log(f_b_c) ** 2) - avg_W(b_cgs) ** 2)
 
 
-def sig_W(b_xgs: np.ndarray) -> float:
-    return np.sqrt(np.sum(p_x_s * (np.log(b_xgs / r_x) ** 2)) - avg_W(b_xgs) ** 2)
-
-
-avg_W_sim = avg_W(avg_prob_phe)
-sig_W_sim = sig_W(avg_prob_phe)
+avg_W_sim = avg_W(avg_prob_phe.T)
+sig_W_sim = sig_W(avg_prob_phe.T)
 
 print(avg_W_sim, sig_W_sim)
 
