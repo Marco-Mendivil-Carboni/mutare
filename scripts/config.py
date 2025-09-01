7235830d8b548480aadc1c041424df0188c39cc4
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-09-01 14:04:07 +0200

    Improve plot a little bit.

--- scripts/config.py
+++ scripts/config.py
@@ -33,18 +33,18 @@ DEFAULT_CONFIG: Config = {
     "model": {
         "n_env": 2,
         "n_phe": 2,
-        "prob_trans_env": [[0.999, 0.001], [0.001, 0.999]],
-        "prob_rep": [[0.006, 0.0], [0.0, 0.004]],
+        "prob_trans_env": [[0.99, 0.01], [0.01, 0.99]],
+        "prob_rep": [[0.010, 0.0], [0.0, 0.008]],
         "prob_dec": [[0.0, 0.0], [0.0, 0.0]],
-        "prob_mut": 0.001,
+        "prob_mut": 0.0001,
         "std_dev_mut": 0.1,
     },
     "init": {
-        "n_agt": 1024,
+        "n_agt": 256,
         "prob_phe": [0.5, 0.5],
     },
     "output": {
-        "steps_per_save": 1024,
+        "steps_per_save": 4096,
         "saves_per_file": 256,
     },
 }
--- scripts/run_all_sims.py
+++ scripts/run_all_sims.py
@@ -20,6 +20,9 @@ mpl.rcParams["figure.constrained_layout.use"] = True
 
 fig, ax = plt.subplots()
 
+ax.set_xlabel("$\\langle\\Delta N\\rangle$")
+ax.set_ylabel("$\\sigma_{\\Delta N}$")
+
 sim_dir = Path("simulations/with_mut/")
 sim_dir.mkdir(parents=True, exist_ok=True)
 
@@ -36,16 +39,19 @@ std_dev = results["n_agt_diff"][0]["std_dev"]
 sem = results["n_agt_diff"][0]["sem"]
 n_eff = (std_dev / sem) ** 2
 avg_W = mean
-avg_W_err = sem
 sig_W = std_dev
+avg_W_err = sem
 sig_W_err = std_dev / np.sqrt(2 * (n_eff - 1))
-
-ax.errorbar(avg_W, sig_W, avg_W_err, sig_W_err, c="r")
 
 n_sims = 16
 prob_phe_l = list(np.linspace(0, 1, num=n_sims))
 
 config["model"]["prob_mut"] = 0.0
+
+avg_W_l = []
+sig_W_l = []
+avg_W_err_l = []
+sig_W_err_l = []
 
 for sim_idx, prob_phe in enumerate(prob_phe_l):
     sim_dir = Path(f"simulations/fixed-{sim_idx:02d}/")
@@ -63,12 +69,16 @@ for sim_idx, prob_phe in enumerate(prob_phe_l):
     std_dev = results["n_agt_diff"][0]["std_dev"]
     sem = results["n_agt_diff"][0]["sem"]
     n_eff = (std_dev / sem) ** 2
-    avg_W = mean
-    avg_W_err = sem
-    sig_W = std_dev
-    sig_W_err = std_dev / np.sqrt(2 * (n_eff - 1))
+    avg_W_l.append(mean)
+    sig_W_l.append(std_dev)
+    avg_W_err_l.append(sem)
+    sig_W_err_l.append(std_dev / np.sqrt(2 * (n_eff - 1)))
 
-    ax.errorbar(avg_W, sig_W, avg_W_err, sig_W_err, c="b")
+ax.errorbar(avg_W_l, sig_W_l, xerr=avg_W_err_l, yerr=sig_W_err_l, c="b", label="fixed")
+
+ax.errorbar(avg_W, sig_W, xerr=avg_W_err, yerr=sig_W_err, c="r", label="with mutations")
+
+ax.legend()
 
 fig.savefig("simulations/test-plot.pdf")
 
