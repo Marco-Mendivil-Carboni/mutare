38be781a856fa6be55671273d775d070632f8c62
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-09-04 08:36:50 +0200

    Improve separation of concerns.

--- scripts/manager.py
+++ scripts/manager.py
@@ -1,32 +1,39 @@
 from multiprocessing import Pool
 from pathlib import Path
 import fcntl
+import contextlib
 from os import cpu_count
 from typing import TypedDict, List
 
 from .config import Config, save_config
 from .runner import run_sim, RunOptions
+from .results import print_all_results
 
 
-class SimTask(TypedDict):
+class SimJob(TypedDict):
     sim_dir: Path
     config: Config
     run_options: RunOptions
 
 
-def make_sim(sim_task: SimTask) -> None:
-    sim_task["sim_dir"].mkdir(parents=True, exist_ok=True)
+def execute_sim_job(sim_job: SimJob) -> None:
+    sim_job["sim_dir"].mkdir(parents=True, exist_ok=True)
 
-    with open(sim_task["sim_dir"] / ".lock", "w") as lock_file:
+    with open(sim_job["sim_dir"] / ".lock", "w") as lock_file:
         fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        with (
+            open(sim_job["sim_dir"] / "output.log", "w", buffering=1) as output_file,
+            contextlib.redirect_stdout(output_file),
+            contextlib.redirect_stderr(output_file),
+        ):
+            save_config(sim_job["config"], sim_job["sim_dir"])
+            run_sim(sim_job["sim_dir"], sim_job["run_options"])
+            print_all_results(sim_job["sim_dir"])
 
-        save_config(sim_task["config"], sim_task["sim_dir"])
-        run_sim(sim_task["sim_dir"], sim_task["run_options"])
 
-
-def make_sims(sim_tasks: List[SimTask]):
+def execute_sim_jobs(sim_jobs: List[SimJob]):
     n_cpus = cpu_count() or 4
     n_processes = max(1, n_cpus - 4)
 
     with Pool(processes=n_processes) as pool:
-        pool.map(make_sim, sim_tasks)
+        pool.map(execute_sim_job, sim_jobs)
--- scripts/runner.py
+++ scripts/runner.py
@@ -1,11 +1,9 @@
 import subprocess
 from pathlib import Path
 from signal import signal, SIGTERM
-import contextlib
 from typing import TypedDict, List, Optional
 from types import FrameType
 
-from .results import print_all_results
 
 stop_requested = False
 
@@ -37,30 +35,23 @@ class RunOptions(TypedDict):
 
 
 def run_sim(sim_dir: Path, run_options: RunOptions) -> None:
-    with (
-        open(sim_dir / "output.log", "w", buffering=1) as output_file,
-        contextlib.redirect_stdout(output_file),
-        contextlib.redirect_stderr(output_file),
-    ):
-        if run_options["clean"]:
-            run_bin(sim_dir, ["clean"])
+    if run_options["clean"]:
+        run_bin(sim_dir, ["clean"])
 
-        n_runs = len(list(sim_dir.glob("run-*")))
-        while n_runs < run_options["n_runs"]:
-            run_bin(sim_dir, ["create"])
-            n_runs += 1
+    n_runs = len(list(sim_dir.glob("run-*")))
+    while n_runs < run_options["n_runs"]:
+        run_bin(sim_dir, ["create"])
+        n_runs += 1
 
-        for run_idx in range(run_options["n_runs"]):
-            run_dir = sim_dir / f"run-{run_idx:04}"
+    for run_idx in range(run_options["n_runs"]):
+        run_dir = sim_dir / f"run-{run_idx:04}"
 
-            n_files = len(list(run_dir.glob("trajectory-*")))
-            while n_files < run_options["n_files"]:
-                if stop_requested:
-                    return
-                run_bin(sim_dir, ["resume", "--run-idx", str(run_idx)])
-                n_files += 1
+        n_files = len(list(run_dir.glob("trajectory-*")))
+        while n_files < run_options["n_files"]:
+            if stop_requested:
+                return
+            run_bin(sim_dir, ["resume", "--run-idx", str(run_idx)])
+            n_files += 1
 
-        if run_options["analyze"]:
-            run_bin(sim_dir, ["analyze"])
-
-        print_all_results(sim_dir)
+    if run_options["analyze"]:
+        run_bin(sim_dir, ["analyze"])
