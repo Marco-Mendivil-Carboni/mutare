50b7fecb9e48cd3ea145e9f66499086f739a5cf1
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-09-18 10:25:04 +0200

    Merge runner and manager and start using new NormModelParams.

--- scripts/explore_configs.py
+++ scripts/explore_configs.py
@@ -4,49 +4,37 @@ from pathlib import Path
 import numpy as np
 
 from utils.config import Config, NormModelParams
-from utils.runner import RunOptions
-from utils.manager import SimJob, execute_sim_jobs
+from utils.runner import RunOptions, SimJob, execute_sim_jobs
 from utils.plotting import make_std_dev_plot
 
 
 if __name__ == "__main__":
-    # base_dir = Path("simulations/explore_configs_2/")
-    base_dir = Path("simulations/explore_configs/")
+    base_dir = Path("simulations/explore_configs_2/")
 
     time_step = 0.01
 
     config: Config = {
-        # "model": NormModelParams(
-        #     n_env=2,
-        #     n_phe=2,
-        #     rate_trans_env=[[-1.0, 1.0], [1.0, -1.0]],
-        #     rate_rep=[[1.2, 0.0], [0.0, 0.8]],
-        #     rate_dec=[[0.0, 1.6], [1.2, 0.0]],
-        #     rate_mut=0.1,
-        #     std_dev_mut=1 / 16,
-        #     time_step=time_step,
-        # ).to_model_params(),
-        "model": {
-            "n_env": 2,
-            "n_phe": 2,
-            "prob_trans_env": [[0.99, 0.01], [0.01, 0.99]],
-            "prob_rep": [[0.012, 0.0], [0.0, 0.008]],
-            "prob_dec": [[0.0, 0.016], [0.012, 0.0]],
-            "prob_mut": 1 / 1024,
-            "std_dev_mut": 1 / 16,
-        },
+        "model": NormModelParams(
+            n_env=2,
+            n_phe=2,
+            rate_trans_env=[[-1.0, 1.0], [1.0, -1.0]],
+            rate_rep=[[1.2, 0.0], [0.0, 0.8]],
+            rate_dec=[[0.0, 1.6], [1.2, 0.0]],
+            rate_mut=0.1,
+            std_dev_mut=1 / 16,
+            time_step=time_step,
+        ).to_model_params(),
         "init": {
             "n_agt": 16384,
             "prob_phe": [0.5, 0.5],
         },
         "output": {
             "steps_per_file": 262144,
-            "steps_per_save": 4096,
-            # "steps_per_save": int(16 / time_step),
+            "steps_per_save": int(64 / time_step),
         },
     }
 
-    run_options = RunOptions(clean=False, n_runs=1, n_files=64, analyze=False)
+    run_options = RunOptions(clean=False, n_runs=1, n_files=16, analyze=True)
 
     sim_jobs = [SimJob.from_config(base_dir, config, run_options)]
 
--- scripts/utils/runner.py
+++ scripts/utils/runner.py
@@ -1,14 +1,17 @@
 import subprocess
+import multiprocessing as mp
 from pathlib import Path
 import os
 from datetime import datetime
 from signal import signal, SIGTERM
-import json
+import fcntl
+import sys
 from dataclasses import dataclass
+from enum import Enum, auto
 from typing import List, Optional
 from types import FrameType
 
-from .results import read_results
+from .config import Config, hash_sim_dir
 
 
 def print_process_msg(message: str) -> None:
@@ -78,6 +81,56 @@ def run_sim(sim_dir: Path, run_options: RunOptions) -> None:
         print_process_msg("analyzing all runs")
         run_bin(sim_dir, ["analyze"])
 
-        for run_idx in range(n_runs):
-            results = json.dumps(read_results(sim_dir, run_idx), indent=4)
-            print_process_msg(f"run {run_idx} results:\n{results}")
+
+@dataclass
+class SimJob:
+    sim_dir: Path
+    run_options: RunOptions
+
+    @classmethod
+    def from_config(cls, base_dir: Path, config: Config, run_options: RunOptions):
+        return cls(sim_dir=hash_sim_dir(base_dir, config), run_options=run_options)
+
+
+class JobResult(Enum):
+    FINISHED = auto()
+    STOPPED = auto()
+    FAILED = auto()
+
+
+def execute_sim_job(sim_job: SimJob) -> JobResult:
+    try:
+        print_process_msg(f"job started: {sim_job.sim_dir}")
+
+        with open(sim_job.sim_dir / ".lock", "w") as lock_file:
+            fcntl.flock(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+
+            run_sim(sim_job.sim_dir, sim_job.run_options)
+
+        print_process_msg("job finished")
+        return JobResult.FINISHED
+
+    except StopRequested:
+        print_process_msg("job stopped")
+        return JobResult.STOPPED
+
+    except Exception as exception:
+        print_process_msg(f"job failed:\n{exception}")
+        return JobResult.FAILED
+
+
+def execute_sim_jobs(sim_jobs: List[SimJob]) -> None:
+    set_signal_handler()
+
+    build_bin()
+
+    with mp.Pool(processes=max(1, mp.cpu_count() // 2)) as pool:
+        job_results = pool.map(execute_sim_job, sim_jobs)
+
+    print("simulation jobs have terminated")
+
+    if job_results.count(JobResult.FAILED) > 0:
+        sys.exit(1)
+
+    if job_results.count(JobResult.STOPPED) > 0:
+        sys.exit(0)
