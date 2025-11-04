b4bb92c7172541b530f7001c1a8d47110d426ae0
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-11-04 10:14:37 +0100

    Add notes file.

--- scripts/make_sims.py
+++ scripts/make_sims.py
@@ -9,13 +9,13 @@ from utils.exec import RunOptions, SimJob, execute_sim_jobs
 from utils.plots import make_plots
 
 
-def make_sims_and_plots(base_dir: Path, base_config: Config, run_options: RunOptions):
+def make_sims(base_dir: Path, base_config: Config, run_options: RunOptions):
     sim_jobs: List[SimJob] = []
 
     sim_jobs.append(SimJob(base_dir, base_config, run_options))
 
     config = deepcopy(base_config)
-    strat_phe_0_values = [(i + 1) / 24 for i in range(24)]
+    strat_phe_0_values = [(i + 1) / 16 for i in range(15)]
 
     for strat_phe_0 in strat_phe_0_values:
         config["init"]["strat_phe"] = [strat_phe_0, 1 - strat_phe_0]
@@ -35,15 +35,15 @@ def make_sims_and_plots(base_dir: Path, base_config: Config, run_options: RunOpt
 
 
 if __name__ == "__main__":
-    base_dir = Path("simulations/biological/")
+    base_dir = Path("simulations/default/")
 
     base_config: Config = {
         "model": {
             "n_env": 2,
             "n_phe": 2,
             "rates_trans": [[-1.0, 1.0], [1.0, -1.0]],
-            "rates_birth": [[1.2, 0.6], [0.6, 0.4]],
-            "rates_death": [[0.4, 0.2], [1.0, 0.4]],
+            "rates_birth": [[1.2, 0.0], [0.0, 0.8]],
+            "rates_death": [[0.0, 1.0], [1.0, 0.0]],
             "prob_mut": 0.002,
         },
         "init": {"n_agents": 240},
@@ -54,6 +54,29 @@ if __name__ == "__main__":
         },
     }
 
-    run_options = RunOptions(clean=False, n_runs=4, n_files=64, analyze=True)
+    run_options = RunOptions(clean=False, n_runs=2, n_files=16, analyze=True)
 
-    make_sims_and_plots(base_dir, base_config, run_options)
+    make_sims(base_dir, base_config, run_options)
+
+    # base_dir = Path("simulations/more_noise/")
+
+    # base_config: Config = {
+    #     "model": {
+    #         "n_env": 2,
+    #         "n_phe": 2,
+    #         "rates_trans": [[-1.0, 1.0], [1.0, -1.0]],
+    #         "rates_birth": [[1.4, 0.2], [0.2, 1.0]],
+    #         "rates_death": [[0.2, 1.6], [1.2, 0.2]],
+    #         "prob_mut": 0.002,
+    #     },
+    #     "init": {"n_agents": 240},
+    #     "output": {
+    #         "steps_per_file": 1_048_576,
+    #         "steps_per_save": 256,
+    #         "hist_bins": 16,
+    #     },
+    # }
+
+    # run_options = RunOptions(clean=False, n_runs=4, n_files=64, analyze=True)
+
+    # make_sims(base_dir, base_config, run_options)
