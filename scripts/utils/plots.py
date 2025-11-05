cbdef6dae0ac68b506b5b4c9a2a0a31627a24910
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-11-05 16:49:41 +0100

    Polish heatmap plots a lot.

--- scripts/utils/plots.py
+++ scripts/utils/plots.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import matplotlib as mpl
-import numpy as np
 from matplotlib.figure import Figure
-import matplotlib.colors as mcolors
+import matplotlib.gridspec as gridspec
+import matplotlib.colors as colors
 from typing import Dict, List, Any
 
 from .exec import SimJob
@@ -15,11 +15,11 @@ mpl.rcParams["text.latex.preamble"] = "\\usepackage{lmodern}"
 mpl.rcParams["font.family"] = "lmodern"
 mpl.rcParams["font.size"] = 11
 
-cm = 1 / 2.54
+CM = 1 / 2.54
 
 mpl.rcParams["figure.constrained_layout.use"] = True
 
-colors = [
+COLORS = [
     "#df591f",
     "#d81e2c",
     "#d21e6f",
@@ -34,9 +34,9 @@ colors = [
     "#15992c",
 ]
 
-plot_style: Dict[str, Any] = dict(ls=":", marker="o", markersize=2)
-fill_style: Dict[str, Any] = dict(lw=0.0, alpha=0.5)
-line_style: Dict[str, Any] = dict(ls=":", lw=1.0, alpha=0.5)
+PLOT_STYLE: Dict[str, Any] = dict(ls=":", marker="o", markersize=2)
+FILL_STYLE: Dict[str, Any] = dict(lw=0.0, alpha=0.5)
+LINE_STYLE: Dict[str, Any] = dict(ls=":", lw=1.0, alpha=0.5)
 
 
 def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
@@ -44,29 +44,34 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
 
     sim_type = avg_analyses["sim_type"]
 
-    fig_1 = Figure(figsize=(16.0 * cm, 10.0 * cm))
+    fig_1 = Figure(figsize=(16.0 * CM, 10.0 * CM))
     ax_1 = fig_1.add_subplot()
     ax_1.set_xlabel("$\\langle\\mu\\rangle$")
     ax_1.set_ylabel("$r_e$")
     ax_1.set_yscale("log")
 
-    fig_2 = Figure(figsize=(16.0 * cm, 10.0 * cm))
+    fig_2 = Figure(figsize=(16.0 * CM, 10.0 * CM))
     ax_2 = fig_2.add_subplot()
-    ax_2.set_xlabel("$p_{\\phi}(0)_i$")
+    ax_2.set_xlabel("$s(0)_i$")
     ax_2.set_ylabel("$\\langle\\mu\\rangle$")
 
-    fig_3 = Figure(figsize=(12.0 * cm, 10.0 * cm))
+    fig_3 = Figure(figsize=(12.0 * CM, 10.0 * CM))
     ax_3 = fig_3.add_subplot()
-    ax_3.set_xlabel("$p_{\\phi}(0)_i$")
-    ax_3.set_ylabel("$\\langle p_{\\phi}(0)\\rangle$")
+    ax_3.set_xlabel("$s(0)_i$")
+    ax_3.set_ylabel("$\\langle s(0)\\rangle$")
 
-    fig_4 = Figure(figsize=(12.0 * cm, 10.0 * cm))
-    ax_4 = fig_4.add_subplot()
-    ax_4.set_xlabel("$p_{\\phi}(0)_i$")
-    ax_4.set_ylabel("$p_{\\phi}(0)$")
+    fig_4 = Figure(figsize=(16.0 * CM, 10.0 * CM))
+    gs = gridspec.GridSpec(1, 3, figure=fig_4, width_ratios=[64, 8, 1], wspace=0)
+    ax_4_l = fig_4.add_subplot(gs[0, 0])
+    ax_4_r = fig_4.add_subplot(gs[0, 1])
+    ax_4_c = fig_4.add_subplot(gs[0, 2])
+    ax_4_l.set_ylabel("$s(0)$")
+    ax_4_l.set_xlabel("$s(0)_i$")
+    ax_4_r.set_xlabel("random init")
+    ax_4_r.set_xticks([])
+    ax_4_r.set_yticks([])
 
     max_mu_strat: Any = None
-    heatmap = list()
     hist_bins = len(
         [
             col
@@ -74,12 +79,15 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
             if "dist_strat_phe_0_" in col[0] and "mean" in col[1]
         ]
     )
-    cmap = mcolors.LinearSegmentedColormap.from_list("custom", ["white", colors[1]])
+    hm_x = list()
+    hm_z1 = list()
+    hm_z2 = list()
+    cmap = colors.LinearSegmentedColormap.from_list("custom", ["white", COLORS[1]])
 
     for avg_analyses, color, label in [
-        (avg_analyses[sim_type == "fixed"], colors[1], "fixed"),
-        (avg_analyses[sim_type == "with mutations"], colors[7], "with mutations"),
-        (avg_analyses[sim_type == "random init"], colors[11], "random init"),
+        (avg_analyses[sim_type == "fixed"], COLORS[1], "fixed"),
+        (avg_analyses[sim_type == "with mutations"], COLORS[7], "with mutations"),
+        (avg_analyses[sim_type == "random init"], COLORS[11], "random init"),
     ]:
         ax_1.errorbar(
             avg_analyses[("growth_rate", "mean")],
@@ -88,13 +96,13 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
             yerr=avg_analyses[("extinct_rate", "sem")],
             c=color,
             label=label,
-            **plot_style,
+            **PLOT_STYLE,
         )
         ax_1.axvline(
-            avg_analyses[("growth_rate", "mean")].mean(), c=color, **line_style
+            avg_analyses[("growth_rate", "mean")].mean(), c=color, **LINE_STYLE
         )
         ax_1.axhline(
-            avg_analyses[("extinct_rate", "mean")].mean(), c=color, **line_style
+            avg_analyses[("extinct_rate", "mean")].mean(), c=color, **LINE_STYLE
         )
 
         if label == "fixed":
@@ -109,7 +117,7 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
                 yerr=avg_analyses[("growth_rate", "sem")],
                 c=color,
                 label=label,
-                **plot_style,
+                **PLOT_STYLE,
             )
         else:
             for growth_rate_mean, growth_rate_sem in avg_analyses[
@@ -120,7 +128,7 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
                     growth_rate_mean + growth_rate_sem,
                     growth_rate_mean - growth_rate_sem,
                     color=color,
-                    **fill_style,
+                    **FILL_STYLE,
                 )
 
         if label != "random init":
@@ -129,7 +137,7 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
                 avg_analyses[("avg_strat_phe_0", "mean")],
                 c=color,
                 label=label,
-                **plot_style,
+                **PLOT_STYLE,
             )
             ax_3.fill_between(
                 avg_analyses["strat_phe_0"],
@@ -138,7 +146,7 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
                 avg_analyses[("avg_strat_phe_0", "mean")]
                 + avg_analyses[("std_dev_strat_phe", "mean")],
                 color=color,
-                **fill_style,
+                **FILL_STYLE,
             )
         else:
             for avg_strat_phe_0, std_dev_strat_phe in avg_analyses[
@@ -149,24 +157,38 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
                     avg_strat_phe_0 + std_dev_strat_phe,
                     avg_strat_phe_0 - std_dev_strat_phe,
                     color=color,
-                    **fill_style,
+                    **FILL_STYLE,
                 )
-        if label == "with mutations":
-            for bin in reversed(range(hist_bins)):
-                heatmap.append(avg_analyses[(f"dist_strat_phe_0_{bin}", "mean")])
 
-    heatmap = np.array([heatmap])
-    im = ax_4.imshow(
-        heatmap[0],
+        if label == "with mutations":
+            hm_x.append(avg_analyses["strat_phe_0"])
+            for bin in range(hist_bins):
+                hm_z1.append(avg_analyses[(f"dist_strat_phe_0_{bin}", "mean")])
+        elif label == "random init":
+            for bin in range(hist_bins):
+                hm_z2.append(avg_analyses[(f"dist_strat_phe_0_{bin}", "mean")])
+
+    im = ax_4_l.pcolormesh(
+        hm_x,
+        [(i + 0.5) / hist_bins for i in range(hist_bins)],
+        hm_z1,
         cmap=cmap,
         vmin=0.0,
         vmax=1.0,
-        # fix this... use pcolormesh
-        extent=(0.5 / hist_bins, 1.0 - 0.5 / hist_bins, 0.0, 1.0),
+        shading="nearest",
     )
 
-    cbar = fig_4.colorbar(im, ax=ax_4, aspect=64, pad=1 / 64)
-    cbar.ax.set_ylabel("$p(p_{\\phi}(0))$")
+    _ = ax_4_r.pcolormesh(
+        [0.0, 1.0],
+        [i / hist_bins for i in range(hist_bins + 1)],
+        hm_z2,
+        cmap=cmap,
+        vmin=0.0,
+        vmax=1.0,
+    )
+
+    cbar = fig_4.colorbar(im, cax=ax_4_c, aspect=64)
+    cbar.ax.set_ylabel("$p(s(0))$")
 
     ax_1.legend()
     fig_1.savefig(fig_dir / "extinct_rate.pdf")
@@ -179,7 +201,8 @@ def make_plots(sim_jobs: List[SimJob], fig_dir: Path) -> None:
     ax_3.legend()
     fig_3.savefig(fig_dir / "avg_strat_phe.pdf")
 
-    ax_4.axhline(max_mu_strat, color="gray", ls="-.")
+    ax_4_l.axhline(max_mu_strat, color="gray", ls="-.")
+    ax_4_r.axhline(max_mu_strat, color="gray", ls="-.")
     fig_4.savefig(fig_dir / "dist_strat_phe.pdf")
 
     print("plots made")
