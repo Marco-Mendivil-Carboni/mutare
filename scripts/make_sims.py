f08488a4d24b9852332ae16c7e3278c2e3ca6d11
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2025-11-07 09:38:12 +0100

    Extend simulations and prepare new ones.

--- scripts/make_sims.py
+++ scripts/make_sims.py
@@ -35,9 +35,11 @@ def make_sims(base_dir: Path, base_config: Config, run_options: RunOptions):
 
 
 if __name__ == "__main__":
-    base_dir = Path("simulations/default/")
+    sims_dir = Path("simulations")
 
-    base_config: Config = {
+    default_dir = sims_dir / "default"
+
+    default_config: Config = {
         "model": {
             "n_env": 2,
             "n_phe": 2,
@@ -57,36 +59,26 @@ if __name__ == "__main__":
     run_options = RunOptions(
         clean=False,
         n_runs=4,
-        n_files=64,
+        n_files=96,
         analyze=True,
     )
 
-    make_sims(base_dir, base_config, run_options)
+    make_sims(default_dir, default_config, run_options)
 
-    base_dir = Path("simulations/biological/")
+    biological_dir = sims_dir / "biological"
 
-    base_config: Config = {
-        "model": {
-            "n_env": 2,
-            "n_phe": 2,
-            "rates_trans": [[-1.0, 1.0], [1.0, -1.0]],
-            "rates_birth": [[1.0, 0.2], [0.0, 0.0]],
-            "rates_death": [[0.0, 0.0], [1.0, 0.1]],
-            "prob_mut": 0.001,
-        },
-        "init": {"n_agents": 100},
-        "output": {
-            "steps_per_file": 1_048_576,
-            "steps_per_save": 256,
-            "hist_bins": 16,
-        },
-    }
+    biological_config = deepcopy(default_config)
+    biological_config["model"]["rates_birth"] = [[1.0, 0.2], [0.0, 0.0]]
+    biological_config["model"]["rates_death"] = [[0.0, 0.0], [1.0, 0.1]]
+    biological_config["model"]["prob_mut"] = 0.001
+    biological_config["init"]["n_agents"] = 100
 
-    run_options = RunOptions(
-        clean=False,
-        n_runs=4,
-        n_files=64,
-        analyze=True,
-    )
+    make_sims(biological_dir, biological_config, run_options)
 
-    make_sims(base_dir, base_config, run_options)
+    # extended_dir = sims_dir / "extended"
+
+    # extended_config = deepcopy(default_config)
+    # extended_config["model"]["prob_mut"] = 0.001
+    # extended_config["init"]["n_agents"] = 960
+
+    # make_sims(extended_dir, extended_config, run_options)
