536de3573c8639ee6cc83f1e7f9eb22795c6e835
Marco-Mendivil-Carboni <ocrammarco01@gmail.com>
2026-01-28 11:25:26 +0100

    Finish scaling plots.

--- scripts/utils/plots.py
+++ scripts/utils/plots.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.optimize import curve_fit
 from shutil import rmtree
 import matplotlib as mpl
 from matplotlib.figure import Figure
 from matplotlib.axes import Axes
 from matplotlib.gridspec import GridSpec
-from matplotlib.colors import PowerNorm
-from typing import Any
+from matplotlib.colors import PowerNorm, LogNorm
+from matplotlib.cm import ScalarMappable
+from typing import Any, cast
 
 from .exec import SimJob, print_process_msg
 from .analysis import SimType, collect_avg_analyses, collect_run_time_series
@@ -67,6 +69,7 @@ COL_TEX_LABELS: dict[str, str] = {
     "dist_strat_phe_0": "$s(A)$",
     "dist_phe_0": "$p(A)$",
     "fitness": "$w$",
+    "alpha": "$\\alpha$",
 }
 
 
@@ -78,7 +81,7 @@ def create_standard_figure(x_col: str, y_col: str) -> tuple[Figure, Axes]:
     return fig, ax
 
 
-def create_heatmap_figure(
+def create_colorbar_figure(
     x_col: str, y_col: str, two_panels: bool
 ) -> tuple[Figure, list[Axes]]:
     fig = Figure(figsize=FIGSIZE)
@@ -279,8 +282,8 @@ def generate_param_plots(param: str, df: pd.DataFrame, job: SimJob) -> None:
     fig_1, ax_1 = create_standard_figure(param, "avg_growth_rate")
     fig_2, ax_2 = create_standard_figure(param, "extinct_rate")
     fig_3, ax_3 = create_standard_figure("avg_growth_rate", "extinct_rate")
-    fig_4, axs_4 = create_heatmap_figure(param, "dist_n_agents", two_panels)
-    fig_5, axs_5 = create_heatmap_figure(param, "dist_strat_phe_0", two_panels)
+    fig_4, axs_4 = create_colorbar_figure(param, "dist_n_agents", two_panels)
+    fig_5, axs_5 = create_colorbar_figure(param, "dist_strat_phe_0", two_panels)
     fig_6, ax_6 = create_standard_figure(param, "avg_strat_phe_0")
     fig_7, ax_7 = create_standard_figure(param, "dist_phe_0")
     fig_8, ax_8 = create_standard_figure(param, "fitness")
@@ -395,6 +398,98 @@ def generate_time_series_plots(df: pd.DataFrame, job: SimJob) -> None:
         fig.savefig(fig_dir / f"{y_col}.pdf")
 
 
+def generate_scaling_plots(df: pd.DataFrame, job: SimJob) -> None:
+    scaling_df = df[df["prob_mut"] == 0].sort_values("strat_phe_0_i")
+    if scaling_df["n_agents_i"].nunique() < 2:
+        return
+
+    fig_1, axs_1 = create_colorbar_figure("strat_phe_0_i", "avg_growth_rate", False)
+    fig_2, axs_2 = create_colorbar_figure("strat_phe_0_i", "extinct_rate", False)
+    fig_3, axs_3 = create_colorbar_figure("strat_phe_0_i", "dist_phe_0", False)
+    fig_4, axs_4 = create_colorbar_figure("strat_phe_0_i", "fitness", False)
+
+    fig_5, axs_5 = create_colorbar_figure("n_agents_i", "extinct_rate", False)
+    fig_6, ax_6 = create_standard_figure("strat_phe_0_i", "alpha")
+
+    norm = LogNorm(
+        vmin=scaling_df["n_agents_i"].min() / 2.0,
+        vmax=scaling_df["n_agents_i"].max() * 2.0,
+    )
+
+    for n_agents_i, subgroup_df in scaling_df.groupby("n_agents_i"):
+
+        def plot_mean_and_uncertainty(ax: Axes, y_col: str) -> None:
+            color = CMAP(norm(cast(float, n_agents_i)))
+            x = subgroup_df["strat_phe_0_i"]
+            y = subgroup_df[(y_col, "mean")]
+            yerr = subgroup_df[(y_col, "sem")]
+            ax.errorbar(x, y, yerr, None, c=color, **PLOT_STYLE)
+
+        plot_mean_and_uncertainty(axs_1[0], "avg_growth_rate")
+        plot_mean_and_uncertainty(axs_2[0], "extinct_rate")
+        plot_mean_and_uncertainty(axs_3[0], "dist_phe_0")
+        plot_mean_and_uncertainty(axs_4[0], "fitness")
+
+    for ax in [axs_2[0]]:
+        ax.set_yscale("log")
+
+    sm = ScalarMappable(norm=norm, cmap=CMAP)
+    for fig, axs in [(fig_1, axs_1), (fig_2, axs_2), (fig_3, axs_3), (fig_4, axs_4)]:
+        cbar = fig.colorbar(sm, cax=axs[1], aspect=64)
+        cbar.ax.set_ylabel(COL_TEX_LABELS["n_agents_i"])
+
+    scaling_df = scaling_df.sort_values("n_agents_i")
+
+    def power_law(
+        x: float | pd.Series, alpha: float, x0: float, A: float
+    ) -> float | pd.Series:
+        return (A * (x + x0)) ** (-alpha)
+
+    strat_phe_0_i_list = []
+    alpha_list = []
+    alpha_err_list = []
+
+    for strat_phe_0_i, subgroup_df in scaling_df.groupby("strat_phe_0_i"):
+        color = CMAP(cast(float, strat_phe_0_i))
+        x = subgroup_df["n_agents_i"]
+        y = subgroup_df[("extinct_rate", "mean")]
+        yerr = subgroup_df[("extinct_rate", "sem")]
+        axs_5[0].errorbar(x, y, yerr, None, c=color, **PLOT_STYLE)
+
+        x, y, yerr = x[y != 0], y[y != 0], yerr[y != 0]
+        strat_phe_0_i_list.append(strat_phe_0_i)
+        popt, pcov = curve_fit(power_law, x, y, p0=(1.0, 10.0, 0.1), sigma=yerr)
+        axs_5[0].errorbar(x, power_law(x, *popt), ls=":", **LINE_STYLE)
+        alpha_list.append(popt[0])
+        alpha_err_list.append(np.sqrt(np.diag(pcov)[0]))
+
+    ax_6.errorbar(
+        strat_phe_0_i_list,
+        alpha_list,
+        alpha_err_list,
+        c=SIM_COLORS[SimType.FIXED],
+        label=SIM_LABELS[SimType.FIXED],
+        **PLOT_STYLE,
+    )
+
+    axs_5[0].set_xscale("log")
+    axs_5[0].set_yscale("log")
+
+    sm = ScalarMappable(cmap=CMAP)
+    cbar = fig_5.colorbar(sm, cax=axs_5[1], aspect=64)
+    cbar.ax.set_ylabel(COL_TEX_LABELS["strat_phe_0_i"])
+
+    fig_dir = job.base_dir / "plots" / "scaling"
+    fig_dir.mkdir(parents=True, exist_ok=True)
+
+    fig_1.savefig(fig_dir / "avg_growth_rate.pdf")
+    fig_2.savefig(fig_dir / "extinct_rate.pdf")
+    fig_3.savefig(fig_dir / "dist_phe_0.pdf")
+    fig_4.savefig(fig_dir / "fitness.pdf")
+    fig_5.savefig(fig_dir / "extinct_rate_scaling.pdf")
+    fig_6.savefig(fig_dir / "alpha.pdf")
+
+
 def plot_sim_jobs(sim_jobs: list[SimJob]) -> None:
     avg_analyses = collect_avg_analyses(sim_jobs)
     init_sim_job = sim_jobs[0]
@@ -407,5 +502,7 @@ def plot_sim_jobs(sim_jobs: list[SimJob]) -> None:
     generate_param_plots("n_agents_i", avg_analyses, init_sim_job)
 
     generate_time_series_plots(run_time_series, init_sim_job)
+
+    generate_scaling_plots(avg_analyses, init_sim_job)
 
     print_process_msg("finished plots")
